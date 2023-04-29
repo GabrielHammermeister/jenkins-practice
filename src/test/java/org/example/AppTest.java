72badeb644d0a8624436706a5272ba53761912bf
Gabriel Hammer <gabriel.hammer523@gmail.com>
2023-04-28 21:59:27 -0300

    configura jacoco

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,46 +1,17 @@
 package org.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Assert;
+import org.junit.Test;
 
 import java.util.ArrayList;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
-    @org.junit.Test
+    @Test
     public void testCompany()
     {
         Company company = new Company(new ArrayList<>(), "company");
-        assertTrue( true );
+        company.hireEmployee(new Employee("Hammer", 18));
+        Assert.assertTrue(true);
     }
 }
